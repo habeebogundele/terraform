0718775e6d0d4116aab6aa8e527318dd010262ce
Habeeb <ogundelehabeeb01@gmail.com>
2026-06-27 07:52:22 +0100

    Terraform setup

---
+++ environments/dev/main.tf
@@ -0,0 +1,14 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+
+  cidr_block = var.cidr_block
+
+  availability_zones = var.availability_zones
+
+  public_subnet_cidrs = var.public_subnet_cidrs
+
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
