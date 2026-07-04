3c41d44b586b6855c36806023d8b558bd4c59628
Habeeb <ogundelehabeeb01@gmail.com>
2026-07-04 01:37:26 +0100

    Add iam user

--- main.tf
+++ main.tf
@@ -20,3 +20,11 @@ module "security_groups" {
   environment  = var.environment
   vpc_id       = module.vpc.vpc_id
 }
+
+module "iam" {
+  source = "./modules/iam"
+
+  project_name = var.project_name
+  environment  = var.environment
+}
+
